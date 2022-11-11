1b2da5e8d28a9fd58ee5b98aa080839581732f4b
Grahambone37 <electricgrunt37@gmail.com>
2022-11-11 14:10:39 -0800

    set up properties and methods

--- mecurySedan.js
+++ mecurySedan.js
@@ -2,5 +2,37 @@
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
+/*
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+*/
+
+//I can't rely on node.js, since I have Windows, but for this project I'll pretend
+
+class Car extends VehicleModule.Vehicle {
+    constructor (make, model, year, color, mileage, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService) {
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = maximumPassengers
+        this.passenger = passenger
+        this.numberOfWheels = numberOfWheels
+        this.maximumSpeed = maximumSpeed
+        this.fuel = fuel
+        this.scheduleService = scheduleService
+    }
+
+    loadPassenger(num) {
+
+    }
+
+    start() {
+
+    }
+
+    scheduleService(mileage) {
+
+    }
+}
+
+
+
+let aTwentyEight = new Car("Mecury", "Sedan", "1965", "blue", 200000, 5, 0, 4, 160, 10, false)
